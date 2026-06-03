d01a5a5e8909a9741568005d0197918fb4fe1a81
catshanddev <catshanddev@gmail.com>
2026-06-03 09:13:13 +0900

    fix: remove commented-out code in PrintablePickingList component

--- src/components/PrintablePickingList.tsx
+++ src/components/PrintablePickingList.tsx
@@ -74,10 +74,9 @@ const PrintablePickingList = React.forwardRef<HTMLDivElement, PrintableProps>(
           <div className="grid-input col-span2 fs16">{uniqueOrderCount}</div>
 
           {/* Group 3: 梱包 */}
-          <div className="grid-label col-span1">時間</div>
-          <div className="grid-input col-span4 fs16"></div>
+          {/* <div className="grid-input col-span4 fs16"></div>
           <div className="grid-label col-span1">個数</div>
-          <div className="grid-input col-span2 fs16">{uniqueOrderCount}</div>
+          <div className="grid-input col-span2 fs16">{uniqueOrderCount}</div> */}
         </div>
 
         {/* 2.5 梱包サイズ集計エリア */}
